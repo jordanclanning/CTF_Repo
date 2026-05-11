f89b2d494d86673940c2f3f0f752018af71d4b0f
Jordan Lanning <jordan.c.lanning@gmail.com>
2026-05-10 23:21:37 -0400

    Update build_pdf_meta.py

--- Testing/PDFs/build_pdf_meta.py
+++ Testing/PDFs/build_pdf_meta.py
@@ -1,3 +1,4 @@
+# build_pdf_meta.py
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 import PyPDF2
@@ -6,7 +7,10 @@ def create_meta_challenge(flag, output_path):
     c = canvas.Canvas(output_path, pagesize=letter)
     c.setTitle("Quarterly Report")
     c.setAuthor("Jordan. C. Lanning, M.I.T., M.P.A.")
-    c.setSubject(ECE_5540_Project9)           # hidden here
-    c.setKeywords("Cybersecurtiy,q3")  # or here, encoded
+    c.setSubject(flag)              # hidden here
+    c.setKeywords("Cybersecurity,q3")  # or here, encoded
     c.drawString(100, 750, "Q3 Cyber Assessment - Nothing to see here.")
     c.save()
+
+# ---- YOUR FLAG GOES HERE ----
+create_meta_challenge("CTF{start_with_META_DATA}", "quarterly_report.pdf")
